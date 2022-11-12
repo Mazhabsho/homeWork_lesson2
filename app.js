081c99729d2623b8f392378587586ebb7dee4028
Solihov Mazhabsho <105422965+Mazhabsho@users.noreply.github.com>
2022-11-12 16:44:01 +0500

    string js

---
+++ app.js
@@ -0,0 +1,56 @@
+/*let a = 'Hello Mazhabsho';
+let b = a.charAt(6)
+console.log(b);
+//CONCAT
+let a = 'Hello';
+let b = 'World!';
+let s = 'Mazhabsho'
+let  c = a.concat(' ', b, ' ',a,' ', s);
+console.log(c);
+
+//replace
+let a = 'Visit Dushanbe';
+let b = a.replace("Dushanbe", "New Your");
+console.log(b);*/
+
+//split
+
+let d = 'Welcome to Dushanbe, i hope you ador to it nuture';
+let s = d.split(' ');
+console.log(s);
+
+//substr
+let a = 'Hllo te World of js';
+let b = a.substr(3, 10);
+console.log(b);
+
+let f = a.substr(-10, 10);
+console.log(f);
+
+//sunsring
+
+let g = 'Hello my friends';
+let j = g.substring(5, 19);
+console.log(j);
+
+
+//slice
+
+let n = 'new world of js is waiting you'
+let k = n.slice(4, 26);
+console.log(k);
+
+//trim()
+let l = '      i love my mother    '
+let result = l.trim();
+console.log(result);
+
+//include()
+let o = 'Hello Mazhabsho,welcome to the universe';
+let h = o.includes("to");
+console.log(h);
+
+//search
+let text = 'Open the window, dlel ';
+let w = text.search('dlel');
+console.log(w);
